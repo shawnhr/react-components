861483600b9834f5f3740ae5953a872cd73e419c
shawnhr <shawnk0818@gmail.com>
2018-02-03 17:45:13 -0800

    Completed

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList items={['Apple', 'Banana']}/>
+  </div>
+);
+
+var GroceryList = (props) => {
+ 
+  
+  return (<div>
+    <ul>
+      {props.items.map(item =>
+        <GroceryListItem item={item}/>
+      )}
+    </ul>
+  </div>)
+};
+
+class GroceryListItem extends React.Component {
+	constructor(props) {
+
+    super(props)
+    this.state = {
+      bold: false
+    }
+  }
+  
+  toggleBold() {
+    this.setState({
+      bold: !this.state.bold
+    });
+    console.log('hovered')
+  }
+  
+  render() {
+    var style = {
+      fontWeight: this.state.bold ? 'bold' : 'normal'
+    }
+    
+    return (
+      <li style={style} onMouseOver={this.toggleBold.bind(this)} onMouseOut={this.toggleBold.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
